64b4e1254dbc39fa05b3e0248efe8af98d58c165
Marina Ivanova <Marjanja7@mail.ru>
2024-02-03 22:04:32 +0300

    проблема со списком

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,7 +6,7 @@ return (
     <div className='max-w-7xl mx-auto shadow-md rounded-md px-6 py-2'>
     <div className="flex justify-between px-10 bg-green-100">
         <nav className='gap-8 justify-between text-center flex mx-auto my-12 text-2xl font-extrabold tracking-tight text-gray-700'>
-            <Link to='/restaurant/:slug'>Меню</Link>
+            {/* <Link to='/restaurant/:slug'>Рестораны</Link> */}
             <Link to='/'>Главная страница</Link>
         </nav>
     </div>
